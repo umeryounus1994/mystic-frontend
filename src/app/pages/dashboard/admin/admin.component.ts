4c90187788d309b2e39e2f2eb42b26296d117d05
umeryounus1994 <umeryounus92@gmail.com>
2024-05-13 23:59:22 +0500

    analytics

--- src/app/pages/dashboard/admin/admin.component.ts
+++ src/app/pages/dashboard/admin/admin.component.ts
@@ -17,7 +17,7 @@ export class AdminComponent implements OnInit {
     users: 0,
     missions: 0,
     quests: 0,
-    treasurehunts: 0
+    hunts: 0
   }
   constructor(private sp: NgxSpinnerService, private api: RestApiService) {
   }
@@ -26,78 +26,12 @@ export class AdminComponent implements OnInit {
     // await this.getQuestionsCharts();
     // await this.getCategoriesCharts();
     // await this.getAllCount();
+    await this.getAnalytics();
   }
-  async getQuestionsCharts() {
-    this.api.get('Analyst/getQuestions')
-      .then((response: any) => {
-        if (response.status === "Success") {
-          this.sp.hide();
-          this.QuestionsChart = new Chart("QuestionsChart", {
-            type: 'bar',
-
-            data: {
-              labels: response?.data?.details?.labels,
-              datasets: [
-                {
-                  label: "Questions",
-                  data: response?.data?.details?.questionsData,
-                  backgroundColor: 'lightskyblue'
-                },
-              ]
-            },
-            options: {
-             // aspectRatio: 2.5,
-                  maintainAspectRatio: false,
-    responsive: true,  
-            }
-
-          });
-        }
-      }).catch((error: any) => {
-        this.sp.hide();
-      });
-  }
-  async getCategoriesCharts() {
-    this.api.get('Analyst/getExamCategories')
-      .then((response: any) => {
-        if (response.status === "Success") {
-          this.sp.hide();
-          this.CategoriesChart = new Chart("CategoriesChart", {
-            type: 'pie',
-
-            data: {
-              labels: response?.data?.details?.labels,
-              datasets: [{
-                //label: response?.data?.details?.examsData,
-                data: response?.data?.details?.examsData,
-                hoverOffset: 4
-              }],
-            },
-            
-            options: {
-              responsive: true,
-              maintainAspectRatio: false,
-              plugins: {
-                legend: {
-                  display: true,
-                  position: 'top',
-                  
-                },
-              
-              }
-            },
-
-          });
-        }
-      }).catch((error: any) => {
-        this.sp.hide();
-      });
-  }
-  async getAllCount() {
-    this.api.get('Analyst/GetAll')
+  async getAnalytics() {
+    this.api.get('user/analytics')
       .then((response: any) => {
         this.counters = response.data;
-        this.sp.hide();
       }).catch((error: any) => {
         this.sp.hide();
       });
