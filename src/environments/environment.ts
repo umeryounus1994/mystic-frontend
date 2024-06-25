60c8054a57ff7d5ce33483fb6ef4a4d4aa2114d0
umeryounus1994 <umeryounus92@gmail.com>
2024-06-25 22:40:27 +0500

    Env update1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiURL: 'http://localhost:8002/api/v1',
-  apiURL: 'http://167.172.191.209:8002/api/v1',
+  apiURL: 'https://sea-lion-app-rizuh.ondigitalocean.app/api/v1',
   reportURL: ''
 };
