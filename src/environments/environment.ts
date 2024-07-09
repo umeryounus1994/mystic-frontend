43150b67bc00b20b4daa8b99039c00cb9565e01d
umeryounus1994 <umeryounus92@gmail.com>
2024-07-09 20:58:14 +0500

    frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 //  apiURL: 'http://localhost:8002/api/v1',
-  apiURL: 'https://sea-lion-app-rizuh.ondigitalocean.app/api/v1',
+  apiURL: 'https://hammerhead-app-by2t4.ondigitalocean.app/api/v1',
   reportURL: ''
 };
