7a719f0b50c1a78cf9bb0a16ad0c57443e38f74e
Muhammad Umer <umeryounus92@gmail.com>
2024-07-30 14:24:15 +0500

    url changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiURL: 'http://localhost:8002/api/v1',
-  apiURL: 'https://hammerhead-app-by2t4.ondigitalocean.app/api/v1',
+  apiURL: 'https://oyster-app-2-oesw7.ondigitalocean.app/api/v1',
   reportURL: ''
 };
