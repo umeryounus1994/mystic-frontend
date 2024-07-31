126aac1f1694d81acecd27926f4ea4755b978233
Muhammad Umer <umeryounus92@gmail.com>
2024-07-31 10:30:21 +0500

    12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiURL: 'http://localhost:8002/api/v1',
-  apiURL: 'https://oyster-app-2-oesw7.ondigitalocean.app/api/v1',
+  apiURL: 'https://mystic-backend-bd2556795b35.herokuapp.com/api/v1',
   reportURL: ''
 };
